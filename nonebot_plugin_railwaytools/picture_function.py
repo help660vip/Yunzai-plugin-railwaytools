ef7bb7d9d8a280d8a79c3aaa058bf7a8565e16b2
leaf2006 <yeshuo20061031@outlook.com>
2026-06-20 23:29:29 +0800

    增加对下关站无法访问时的异常提示（之前一直忘记加）

--- nonebot_plugin_railwaytools/picture_function.py
+++ nonebot_plugin_railwaytools/picture_function.py
@@ -24,36 +24,40 @@ async def handle_xiaguanzhan_photo(event:Event, args: Message = CommandArg()): #
 
     if number := args.extract_plain_text():
         await xiaguanzhan_photo.send("正在加载图片，时间可能略久...")
-        async with httpx.AsyncClient(headers=API.headers, timeout=30.0) as client:
-            data = {
-                "keyword": number.lower() # Fix bug:小补丁
-            }
-            xiaguanzhan_resp = await client.post(API.api_xiaguanzhan, data=data)
-            xiaguanzhan_resp.encoding = "gb2312"
-        # 获取基本信息
-        first_match = utils.xiaguanzhan_first_match
-        title = first_match(r'<h1><span class="blue"><a href="ProView\.asp\?ProId=[^"]*" title="[^"]*" target="_blank">(.*?)</a></span></h1>', xiaguanzhan_resp.text)
-        if title == None:
-            await xiaguanzhan_photo.finish(f"暂无{number}的信息")
-        manufacturer = first_match(r'生产厂商：(.*?)<BR>', xiaguanzhan_resp.text)
-        shoot_date = first_match(r'拍摄日期：(.*?)<BR>', xiaguanzhan_resp.text)
-        shoot_author = first_match(r'拍摄作者：(.*?)</FONT>', xiaguanzhan_resp.text)
-        photo_url = first_match(r'下载地址：<a href="(.*?)" target="_blank">', xiaguanzhan_resp.text)
+        try:
+            async with httpx.AsyncClient(headers=API.headers, timeout=30.0) as client:
+                data = {
+                    "keyword": number.lower() # Fix bug:小补丁
+                }
+                xiaguanzhan_resp = await client.post(API.api_xiaguanzhan, data=data)
+                xiaguanzhan_resp.encoding = "gb2312"
+            # 获取基本信息
+            first_match = utils.xiaguanzhan_first_match
+            title = first_match(r'<h1><span class="blue"><a href="ProView\.asp\?ProId=[^"]*" title="[^"]*" target="_blank">(.*?)</a></span></h1>', xiaguanzhan_resp.text)
+            if title == None:
+                await xiaguanzhan_photo.finish(f"暂无{number}的信息")
+            manufacturer = first_match(r'生产厂商：(.*?)<BR>', xiaguanzhan_resp.text)
+            shoot_date = first_match(r'拍摄日期：(.*?)<BR>', xiaguanzhan_resp.text)
+            shoot_author = first_match(r'拍摄作者：(.*?)</FONT>', xiaguanzhan_resp.text)
+            photo_url = first_match(r'下载地址：<a href="(.*?)" target="_blank">', xiaguanzhan_resp.text)
 
-        title_separate = title.split(' ')
-        locomotive_no = title_separate[1]
-        locomotive_allocation = title_separate[2]
+            title_separate = title.split(' ')
+            locomotive_no = title_separate[1]
+            locomotive_allocation = title_separate[2]
 
-        xiaguanzhan_photo_output = Message ([
-            MessageSegment.image(photo_url),
-            f"【{locomotive_no}】\n",
-            f"配属：{locomotive_allocation}\n",
-            f"生产厂商：{manufacturer}\n",
-            f"拍摄日期：{shoot_date}\n",
-            f"拍摄作者：{shoot_author}\n\n",
-            "数据来源：下关站-铁路摄影馆",
-        ])
-        await xiaguanzhan_photo.finish(xiaguanzhan_photo_output)
+            xiaguanzhan_photo_output = Message ([
+                MessageSegment.image(photo_url),
+                f"【{locomotive_no}】\n",
+                f"配属：{locomotive_allocation}\n",
+                f"生产厂商：{manufacturer}\n",
+                f"拍摄日期：{shoot_date}\n",
+                f"拍摄作者：{shoot_author}\n\n",
+                "数据来源：下关站-铁路摄影馆",
+            ])
+            await xiaguanzhan_photo.finish(xiaguanzhan_photo_output)
+        
+        except httpx.ReadTimeout:
+            await xiaguanzhan_photo.finish("无法访问下关站，请稍后再试！")
 
     else:
         await xiaguanzhan_photo.finish("请输入正确的车号!，如：HXD1D-1898")
